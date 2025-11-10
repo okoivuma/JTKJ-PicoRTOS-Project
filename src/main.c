b4ce55d6d7800c2082446a3955626a2421b72f5b
okoivuma <oskarikoivumaki@icloud.com>
2025-11-10 14:06:01 +0200

    pieniä fixejä mainissa

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #define DEFAULT_STACK_SIZE  2048
 
 void sensortask(void *args);
-void printTask(void *args);
+void print_task(void *args);
 enum state { 
     STATE_IDLE,
     STATE_RUNNING,
@@ -33,23 +33,23 @@ enum orientation currentOrientation = pysty;
 int main() {
     stdio_init_all();
     // Uncomment this lines if you want to wait till the serial monitor is connected
-    /*while (!stdio_usb_connected()){
+    while (!stdio_usb_connected()){
         sleep_ms(10);
-    }*/ 
+    }
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
 
     TaskHandle_t sensorTask = NULL;
     TaskHandle_t printTask = NULL;
     // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(sensorTask,       // (en) Task function
+    BaseType_t result = xTaskCreate(sensortask,       // (en) Task function
                 "imu",              // (en) Name of the task 
                 DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
                 NULL,               // (en) Arguments of the task 
                 2,                  // (en) Priority of this task
                 &sensorTask);    // (en) A handle to control the execution of this task
 
-    BaseType_t result2 = xTaskCreate(printTask,       // (en) Task function
+    BaseType_t result2 = xTaskCreate(print_task,       // (en) Task function
                 "print",              // (en) Name of the task 
                 DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
                 NULL,               // (en) Arguments of the task 
@@ -92,9 +92,11 @@ void sensortask(void *args){
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &temp) == 0) {
 
             printf("Accel: X=%f, Y=%f, Z=%f \n", ax, ay, az);
-            if (az > 0.8) {
+            if (az > 0.8 || az < -0.8) {
                 currentOrientation = vaaka;
-            } else if (ay > 0.8) {
+                // Vois asettaa myös x:lle tarkistuksen
+                // toimis myös kylellään
+            } else if (ay > 0.8 || ay < -0.8) {
                 currentOrientation = pysty;
             }
 
@@ -106,7 +108,7 @@ void sensortask(void *args){
     }
 }
 
-void printTask(void *args){
+void print_task(void *args){
     if (currentOrientation == pysty) {
         printf("Orientation: Pysty\n");
     } else if (currentOrientation == vaaka) {

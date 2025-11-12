87c4f1abb6dc860ef89ed55de3821e0ed8c97681
okoivuma <oskarikoivumaki@icloud.com>
2025-11-12 22:40:57 +0200

    lisätty kommentteja , nopeutettu arvojen lukua ja tulostusta

--- src/main.c
+++ src/main.c
@@ -5,10 +5,11 @@
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <task.h>
-
 #include "tkjhat/sdk.h"
 
-
+/*
+** Pittää muistaa kommentoija funktiot
+*/
 #define DEFAULT_STACK_SIZE  2048
 
 // Prototypes of the tasks
@@ -26,6 +27,7 @@ enum state {
 };
 
 enum orientation {
+    // nämä vois kääntää lontooksi
     pysty,
     vaaka,
     kylki
@@ -33,7 +35,7 @@ enum orientation {
 
 enum state programState = STATE_IDLE;
 enum orientation currentOrientation = pysty;
-bool lahetys = false;
+bool button_pressed = false;
 
 
 int main() {
@@ -44,10 +46,14 @@ int main() {
     }
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+    
+    init_button1();
+    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, button_fxn);
+
     programState = STATE_RUNNING;
     TaskHandle_t sensorTask = NULL;
     TaskHandle_t printTask = NULL;
-    TaskHandle_t buttonTask = NULL;
+    
     // Create the tasks with xTaskCreate
     BaseType_t result = xTaskCreate(sensortask,       // (en) Task function
                 "imu",              // (en) Name of the task 
@@ -63,16 +69,6 @@ int main() {
                 2,                  // (en) Priority of this task
                 &printTask);    // (en) A handle to control the execution of this task
 
-    init_button1();
-    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, button_fxn);
-        /*
-    BaseType_t result3 = xTaskCreate(button_task,       // (en) Task
-                "button",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &buttonTask);    // (en) A handle to control the execution of this task*/
-
     if(result != pdPASS || result2 != pdPASS) {
         printf("Task creation failed\n");
         return 0;
@@ -86,7 +82,6 @@ int main() {
 }
 
 void sensortask(void *args){
-    
     
     init_i2c_default();
 
@@ -109,10 +104,9 @@ void sensortask(void *args){
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &temp) == 0) {
 
             //printf("Accel: X=%f, Y=%f, Z=%f \n", ax, ay, az);
+            // vois olla vaikka abs ja arvot vois pistää vakioiksi
             if (az > 0.8 || az < -0.8) {
                 currentOrientation = vaaka;
-                // Vois asettaa myös x:lle tarkistuksen
-                // toimis myös kylellään
                 programState = STATE_orientation_changed;
             } else if (ay > 0.8 || ay < -0.8) {
                 currentOrientation = pysty;
@@ -120,34 +114,30 @@ void sensortask(void *args){
             } else if (ax > 0.8 || ax < -0.8) {
                 currentOrientation = kylki;
                 programState = STATE_orientation_changed;
-                //lahetys = true;
             }
 
         } else {
-            printf("Failed to read imu data\n");
-            
+            printf("Failed to read imu data\n"); // miksi tämä tulee välillä?
+            // State error?
+            // Delete task?
         }
-
-        vTaskDelay(pdMS_TO_TICKS(250));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
-}
-
-void button_task(void *args){
-    for(;;){
-        lahetys = gpio_get(SW1_PIN);
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-   //lahetys = true;
 }
 
 void button_fxn(uint gpio, uint32_t events){
-    lahetys = true;
+    button_pressed = true;
+    // kikkailee ledin jos jaksaa/ehtii
 }
 
 void print_task(void *args){
+    /*
+    ** TODO:
+    ** SERIAL COMMUNICATION!
+    */
     for(;;){
-        vTaskDelay(pdMS_TO_TICKS(500));
-        if (programState == STATE_orientation_changed && lahetys) {
+        vTaskDelay(pdMS_TO_TICKS(50));
+        if (programState == STATE_orientation_changed && button_pressed) {
             if (currentOrientation == pysty) {
             printf(".");
             } else if (currentOrientation == vaaka) {
@@ -157,9 +147,8 @@ void print_task(void *args){
             } else {
             printf("Orientation: Unknown\n");
             }   
-            lahetys = false;
+            button_pressed = false;
             programState = STATE_RUNNING;
         }
     }
-    
 }

3bf45455c4fc000459b02e3384ad5153f94f02fb
okoivuma <oskarikoivumaki@icloud.com>
2025-11-17 17:06:34 +0200

    ledipalaute vaihdettu kokonaan printtaskiin, toimii paremmin

--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 
 #include <stdio.h>
-//#include <string.h>
 #include <pico/stdlib.h>
 #include <FreeRTOS.h>
-//#include <queue.h>
 #include <task.h>
 #include "tkjhat/sdk.h"
 #include <math.h>
-/*
-** Pittää muistaa kommentoija funktiot
-*/
+
+// Define constants
 #define DEFAULT_STACK_SIZE  2048
 #define MESSAGE_BUFFER_SIZE 256
 #define IMU_THRESHOLD 0.8f
@@ -19,14 +16,6 @@ void sensortask(void *args);
 void print_task(void *args);
 void button_fxn(uint gpio, uint32_t events);
 
-
-enum state { 
-    STATE_IDLE,
-    STATE_RUNNING,
-    STATE_orientation_changed,
-    
-    STATE_ERROR
-};
 // Machine orientation variables
 enum orientation {
     VERTICAL,
@@ -34,20 +23,18 @@ enum orientation {
     SIDE
 };
 
-enum state programState = STATE_IDLE;
+// Global variables
+bool orientation_ready = false;     // true when orientation is read from imu sensor
+bool button1_pressed = false;   // flag for button 1 press
+bool button2_pressed = false;   // flag for button 2 press
 enum orientation currentOrientation = VERTICAL;
-bool button1_pressed = false;
-bool button2_pressed = false;
 char morse_message[MESSAGE_BUFFER_SIZE] = "";
 uint8_t morse_index = 0;
 
 
 int main() {
     stdio_init_all();
-    // Uncomment this lines if you want to wait till the serial monitor is connected
-    //while (!stdio_usb_connected()){
-    //    sleep_ms(10);
-    //}
+    
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
     
@@ -59,24 +46,23 @@ int main() {
     gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, button_fxn);
     gpio_set_irq_enabled_with_callback(BUTTON2, GPIO_IRQ_EDGE_RISE, true, button_fxn);
 
-    programState = STATE_RUNNING;
     TaskHandle_t sensorTask = NULL;
     TaskHandle_t printTask = NULL;
     
     // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(sensortask,       // (en) Task function
-                "imu",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &sensorTask);    // (en) A handle to control the execution of this task
+    BaseType_t result = xTaskCreate(sensortask,       
+                "imu",             
+                DEFAULT_STACK_SIZE, 
+                NULL,               
+                2,                  
+                &sensorTask);    
 
-    BaseType_t result2 = xTaskCreate(print_task,       // (en) Task function
-                "print",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE, // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,               // (en) Arguments of the task 
-                2,                  // (en) Priority of this task
-                &printTask);    // (en) A handle to control the execution of this task
+    BaseType_t result2 = xTaskCreate(print_task,       
+                "print",              
+                DEFAULT_STACK_SIZE, 
+                NULL,               
+                2,                  
+                &printTask);   
 
     if(result != pdPASS || result2 != pdPASS) {
         printf("__Task creation failed\n__");
@@ -92,42 +78,32 @@ int main() {
 
 void sensortask(void *args){
     (void) args;
-    //init_i2c_default();
-
+    
     if (init_ICM42670() != 0) {
         printf("__Failed to initialize IMU sensor\n__");
-        vTaskDelete(NULL);
-        return;
     }
 
     if (ICM42670_start_with_default_values() != 0){
-        printf("__ICM-42670P could not initialize accelerometer or gyroscope\n__");
-        vTaskDelete(NULL);
-        return;
+        printf("__Failed to initialize IMU sensor\n__");
     }
 
-    printf("__IMU initialized__\n");
-    
     for (;;) {
         float ax, ay, az, gx, gy, gz, temp;
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &temp) == 0) {
 
-            //printf("Accel: X=%f, Y=%f, Z=%f \n", ax, ay, az);
-            // vois olla vaikka abs ja arvot vois pistää vakioiksi
             if (fabs(az) > IMU_THRESHOLD) {
                 currentOrientation = HORIZONTAL;
-                programState = STATE_orientation_changed;
+                orientation_ready = true;
             } else if (fabs(ay) > IMU_THRESHOLD) {
                 currentOrientation = VERTICAL;
-                programState = STATE_orientation_changed;
+                orientation_ready = true;
             } else if (fabs(ax) > IMU_THRESHOLD) {
                 currentOrientation = SIDE;
-                programState = STATE_orientation_changed;
+                orientation_ready = true;
             }
 
         } else {
-            //printf("Failed to read imu data, please put machine to known state\n"); // miksi tämä tulee välillä?
-            programState = STATE_ERROR;
+            printf("__Failed to read imu data__\n");    
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
@@ -136,10 +112,8 @@ void sensortask(void *args){
 void button_fxn(uint gpio, uint32_t events){
     if (gpio == BUTTON1) {
         button1_pressed = true;
-        toggle_led();
     } else if (gpio == BUTTON2) {
         button2_pressed = true;
-        button1_pressed = true;
     }
 }
 
@@ -147,22 +121,17 @@ void print_task(void *args){
     (void) args;
 
     for(;;){
-        if (programState == STATE_orientation_changed && button1_pressed && !button2_pressed) {
+        if (orientation_ready && button1_pressed && !button2_pressed) {
             if (currentOrientation == VERTICAL) {
-                //printf("__.__");
                 morse_message[morse_index++] = '.';
             } else if (currentOrientation == HORIZONTAL) {
-                //printf("__-__");
                 morse_message[morse_index++] = '-';
             } else if (currentOrientation == SIDE) {
-                //printf("__ __");
                 morse_message[morse_index++] = ' ';
-            } else {
-            printf("Orientation: Unknown\n");
-            }   
+            }
             button1_pressed = false;
-            programState = STATE_RUNNING;
-            toggle_led();
+            orientation_ready = false;
+            blink_led(1);
         } else if (button2_pressed) {
             morse_message[morse_index++] = ' ';
             morse_message[morse_index++] = ' '; 
@@ -175,7 +144,7 @@ void print_task(void *args){
             morse_message[0] = '\0';
             button1_pressed = false;
             button2_pressed = false;
-            programState = STATE_RUNNING;
+            blink_led(2);
         }
         vTaskDelay(pdMS_TO_TICKS(200));
     }

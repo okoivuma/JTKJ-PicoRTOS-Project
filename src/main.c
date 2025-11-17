6810a31483e9bfc75708beb7a9ebea07b3ed411e
okoivuma <oskarikoivumaki@icloud.com>
2025-11-17 17:57:41 +0200

    lisätty kommentteja ja refaktorointia

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
-
+/*
+* Introduction to Computer Systems 2025
+* Morse code transmitter using FreeRTOS on Raspberry Pi Pico
+* Authors: Oskari Koivumäki, Jonne Kemppainen, Joakim Brännlund
+*/
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <FreeRTOS.h>
@@ -27,29 +31,39 @@ enum orientation {
 bool orientation_ready = false;     // true when orientation is read from imu sensor
 bool button1_pressed = false;   // flag for button 1 press
 bool button2_pressed = false;   // flag for button 2 press
-enum orientation currentOrientation = VERTICAL;
-char morse_message[MESSAGE_BUFFER_SIZE] = "";
-int message_index = 0;
+enum orientation currentOrientation = VERTICAL; // current orientation of the machine
+char morse_message[MESSAGE_BUFFER_SIZE] = "";  // buffer for morse message
+int message_index = 0;                  // current index in the message buffer
 
 
+/*
+* Initializes all componenents and creates FreeRTOS tasks and interuprions
+* Tasks: sensortask, print_task
+* Interrupts: button_fxn for button 1 and button 2
+*/
 int main() {
+
+    // Initialize stdio
     stdio_init_all();
     
+    // Initialize HAT SDK, initializes i2c for imu sensor
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
     
+    // Initialize buttons and led
     init_button1();
     init_button2();
 
     init_led();
     
+    // Set up button interrupts
     gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, button_fxn);
     gpio_set_irq_enabled_with_callback(BUTTON2, GPIO_IRQ_EDGE_RISE, true, button_fxn);
 
+    // Create the tasks with xTaskCreate
     TaskHandle_t sensorTask = NULL;
     TaskHandle_t printTask = NULL;
     
-    // Create the tasks with xTaskCreate
     BaseType_t result = xTaskCreate(sensortask,       
                 "imu",             
                 DEFAULT_STACK_SIZE, 
@@ -76,6 +90,11 @@ int main() {
     return 0;
 }
 
+/*
+* Task to read data from imu sensor
+* Uses ICM42670 sensor to read machine orientation
+* Sets flag orientation_ready when new orientation is available from sensor data
+*/
 void sensortask(void *args){
     (void) args;
     
@@ -87,6 +106,7 @@ void sensortask(void *args){
         printf("__Failed to initialize IMU sensor\n__");
     }
 
+    // Infinite loop to read sensor data
     for (;;) {
         float ax, ay, az, gx, gy, gz, temp;
         if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &temp) == 0) {
@@ -109,6 +129,7 @@ void sensortask(void *args){
     }
 }
 
+// Handler for button interrupts
 void button_fxn(uint gpio, uint32_t events){
     if (gpio == BUTTON1) {
         button1_pressed = true;
@@ -117,17 +138,28 @@ void button_fxn(uint gpio, uint32_t events){
     }
 }
 
+/*
+* Task to process and print morse messages
+* Waits for button presses and orientation data to build message
+* Button1 adds morse symbol to message buffer based on machine orientation and increments buffer index
+* Button2 sends the message to client and resets the message buffer and index
+* Blinks led one time for added symbol, two times for sent message
+*/
 void print_task(void *args){
     (void) args;
 
     for(;;){
         if (orientation_ready && button1_pressed) {
-            // check for buffer overflow
+            // check for buffer overflow.
+            // Final message needs space for two spaces, newline and null character
             if (message_index >= MESSAGE_BUFFER_SIZE - 4) {
                 printf("__Message buffer full. Please press button 2 to send message__\n");
                 button1_pressed = false;
+                orientation_ready = false;
+                continue;
             }
 
+            // Add morse symbol to buffer based on orientation
             if (currentOrientation == VERTICAL) {
                 morse_message[message_index++] = '.';
             } else if (currentOrientation == HORIZONTAL) {
@@ -135,21 +167,32 @@ void print_task(void *args){
             } else if (currentOrientation == SIDE) {
                 morse_message[message_index++] = ' ';
             }
+
+            // return to wait for next input
             button1_pressed = false;
             orientation_ready = false;
+
+            // Inform user of added symbol
             blink_led(1);
         } else if (button2_pressed) {
+            // Add the final symbols to match the message protocol
             morse_message[message_index++] = ' ';
             morse_message[message_index++] = ' '; 
             morse_message[message_index++] = '\n';
             morse_message[message_index] = '\0'; 
+
+            // Print the morse message by iterating through the buffer
             for (int i = 0; i < message_index; i++) {
                 printf("%c", morse_message[i]);
             }
+
+            // Reset for next message
             message_index = 0;
             morse_message[0] = '\0';
             button1_pressed = false;
             button2_pressed = false;
+
+            // Inform user of sent message
             blink_led(2);
         }
         vTaskDelay(pdMS_TO_TICKS(200));
